fa35559274e75101555d6b4a2b1753f738feb9fb
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-01-18 17:34:10 -0500

    adicion menu deplegable movil

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+let nav = document.querySelector('.links-contenedor');
+document.querySelector('.boton-menu').addEventListener('click',
+function () {
+    nav.classList.toggle('active');
+});
+
 AOS.init({
     offset:1
 });
+
